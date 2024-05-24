d02d073772dd58d6019d8a9011d093690eda1195
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2024-05-24 23:22:18 +0800

    Create Inhabitant of the Deep Sea_1955D.cpp

---
+++ Codeforces Contest Solutions/Inhabitant of the Deep Sea_1955D.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; // no of test cases
+    for (int l=0;l<t;l++){
+        int n,k; // n is no of ships, k is no of attacks
+        cin >> n >> k;
+        deque <ll> ship(n);
+        for (int i =0;i < n;i++){
+            cin >> ship[i];
+        }
+        int ans = 0;
+        while (ship.size() > 1 && (k>0)) { // k is non zero in this case
+            ll min_no = min(ship.front(),ship.back());
+            if (k < 2*min_no) {
+                ship.front() -= k/2 + k%2;
+                ship.back() -= k/2;
+                k = 0;
+            }
+            else {
+                ship.front() -= min_no;
+                ship.back() -=min_no;
+                k-= 2*min_no;
+            }
+            if (ship.front() == 0) {
+                ship.pop_front();
+                ans++;
+            }
+            if (ship.back() == 0) {
+                ship.pop_back();
+                ans++;
+            }
+        }
+        cout << ans + (ship.front() <= k && ship.size() > 0) << endl;
+    }
+    return 0;
+}
