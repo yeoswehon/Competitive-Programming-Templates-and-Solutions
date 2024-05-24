3ef4cda04047b611a183574a32d72e8e5d9bbe15
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2024-05-24 17:18:07 +0800

    Create Bit++_282A.cpp

---
+++ Codeforces Contest Solutions/Bit++_282A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    int x = 0;
+    string s;
+    while (t--) {
+        cin >> s;
+        if (s[1] == '+') x++;
+        else x--;
+    }
+    cout << x;
+    return 0;
+}
